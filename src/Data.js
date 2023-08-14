8a2172b3a8593321a2edc83a0fc86399dcb67975
mostafa-jamous <mostafa-jamoos@hotmail.com>
2023-08-14 14:57:41 +0300

    Updates

--- src/Data.js
+++
@@ -1,7 +0,0 @@
-export const items = [{id:Math.random(),title:"وجبة إفطار",description:"وجبة إفطار",price:"25000 ل.س",category:"إفطار",img:"1.png"},
-{id:Math.random(),title:"وجبة غداء",description:"وجبة غداء",price:"23000 ل.س",category:"غداء",img:"2.png"},
-{id:Math.random(),title:"وجبة عشاء",description:"وجبة عشاء",price:"30000 ل.س",category:"عشاء",img:"3.png"},
-{id:Math.random(),title:"وجبة غداء",description:"وجبة غداء",price:"33000 ل.س",category:"غداء",img:"2.png"},
-{id:Math.random(),title:"وجبة عشاء",description:"وجبة عشاء",price:"20000 ل.س",category:"عشاء",img:"3.png"},
-{id:Math.random(),title:"وجبة إفطار",description:"وجبة إفطار",price:"18000 ل.س",category:"إفطار",img:"1.png"}]
-
